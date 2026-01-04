78edd6db6012ef6a0bc5a8fcc865b06bddf69d49
Chetan <chetanmittal39@gmail.com>
2026-01-04 19:57:36 +0530

    Service section and About Us Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,38 @@ export default {
       sm: "340px",
       md: "540px",
       lg: "768px",
-      xl: "1180px"
+      xl: "1180px",
     },
-    extend: {},
-    fontFamily: {
-      Jost: ["Jost" , "sans-serif"],
-      Lobster: ["Lobster" , "sans-serif"]
+    extend: {
+      keyframes: {
+        move: {
+          "0%, 100%": { transform: "translateY(0)" },   // start & end at normal position
+          "50%": { transform: "translateY(-1rem)" },    // move up in the middle
+        },
+
+        rotate: {
+          "0%" : {transform: "rotate(0deg)"},
+          "100%": {transform: "rotate(360deg)"},
+        },
+
+        scaleUp: {
+          "0%" : {transform: "scale(0.8)"},
+          "50%" : {transform: "scale(0.8)"},
+          "0%" : {transform: "scale(0.8)"},
+        }
+      },
+
+      animation: {
+        movingY: "move 3s linear infinite",
+        rotating: "rotate 15s linear infinite",
+        scalingUp: "scaleUp 15s linear infinite",
+      },
+
+      
+      fontFamily: {
+        jost: ["Jost", "sans-serif"],
+        lobster: ["Lobster", "sans-serif"],
+      },
     },
   },
   plugins: [],
